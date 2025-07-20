cfbe38cd9efcaf3cb4edd1a694c70bee5a15c06e
Shinhh7777 <shinhh777@g.skku.edu>
2025-07-20 17:06:12 +0900

    test commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,11 @@ import Signup5 from './pages/Signup5';
 import Home from './pages/Home';
 import SellerDetail from './pages/SellerDetail';
 import MenuDetail from './pages/MenuDetail';
+import OtherProfile from './pages/other_user_profile';
+import SellerAuth from './pages/seller_auth';
+import SellerItem from './pages/seller_item';
+import RecipeList from './pages/user_recipe_list';
+import ReviewList from './pages/user_review_list';
 
 // components
 import NavigationBar from './components/NavigationBar';
@@ -38,6 +43,11 @@ const AppRoutes = () => {
         <Route path="/home" element={<Home />} />
         <Route path="/seller/:sellerId" element={<SellerDetail />} />
         <Route path="/menu/:menuId" element={<MenuDetail />} />
+        <Route path="/other_user_profile/:userId" element={<OtherProfile />} />
+        <Route path="/seller_auth/:userId" element={<SellerAuth />} />
+        <Route path="/seller_item/:userId" element={<SellerItem />} />
+        <Route path="/user_recipe_list/:userId" element={<RecipeList />} />
+        <Route path="/user_review_list/:userId" element={<ReviewList />} />
       </Routes>
       {!hideNavigationBar && <NavigationBar />}
     </>
