841377fd28a77c0d89c1b07ace689f2f850322c1
kyumin78 <kyumin7886@gmail.com>
2025-08-01 17:36:52 +0900

    추가 : other_user_profile, seller_auth, user_recipe/review_list 주석처리

--- client/src/pages/seller_auth.jsx
+++ client/src/pages/seller_auth.jsx
@@ -3,10 +3,11 @@ import { useParams, useNavigate } from 'react-router-dom';
 import { sellers } from '../data/sellers';
 
 const SellerAuth = () => {
-    const { userId } = useParams();
-    const user = sellers.find(u => u.id === Number(userId));
+    const { userId } = useParams(); //userId를 받아오는 부분
+    const user = sellers.find(u => u.id === Number(userId));    //userId를 통해 유저 정보를 저장하는 부분
     const navigate = useNavigate();
 
+    //뒤로가기 함수
     const goBack = () => {
         navigate(-1);
     }
@@ -14,31 +15,38 @@ const SellerAuth = () => {
     return (
         <div style={styles.wrapper}>
         <div style={styles.container}>
+            {/*헤더*/}
             <div style={styles.header}>
-            <div style={styles.backButton}><span onClick={goBack}>←</span></div>
-            <div style={styles.headerTitle}>판매자 인증정보</div>
-            <div style={{ width: 18 }} />
+                {/*뒤로가기 버튼*/}
+                <div style={styles.backButton}><span onClick={goBack}>←</span></div>
+                {/*제목*/}
+                <div style={styles.headerTitle}>판매자 인증정보</div>
+                <div style={{ width: 18 }} />
             </div>
+            {/*페이지 내용*/}
             <div style={styles.scrollArea}>
-            <div style={styles.section}>
-                <div style={styles.infoBox}>
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                    <div style={{ fontSize: 30, marginRight: 12 }}>🧑‍🍳</div>
-                    <div>
-                    <div style={styles.sectionTitle}>{user.name} 님의 판매자 인증정보</div>
-                    <div style={{ fontSize: 14, color: '#666' }}>
-                        사업자 인증<br />위생점검
+                <div style={styles.section}>
+                    <div style={styles.infoBox}>
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                        {/*프로필 이미지 부분*/}
+                        <div style={{ fontSize: 30, marginRight: 12 }}>🧑‍🍳</div>
+                        <div>
+                        {/*판매자 닉네임 + 님의 판매자 인증정보*/}
+                        <div style={styles.sectionTitle}>{user.name} 님의 판매자 인증정보</div>
+                        <div style={{ fontSize: 14, color: '#666' }}>
+                            사업자 인증<br />위생점검
+                        </div>
+                        </div>
                     </div>
                     </div>
                 </div>
+                {/*판매자 인증 내용 부분, 판매자 인증 요소들 정해지면 users 데이터 기반으로 어떤 내용들을 띄워줄지 결정.*/}
+                <div style={{ ...styles.infoBox, fontSize: 13, color: '#444' }}>
+                    먼저 방문해서 확인했어요.<br />
+                    정기 위생 교육을 이수했어요.<br />
+                    실제 등록된 사업자로 인증했어요.<br />
+                    공용 공간이 아닌 별도 주방이에요.
                 </div>
-            </div>
-            <div style={{ ...styles.infoBox, fontSize: 13, color: '#444' }}>
-                먼저 방문해서 확인했어요.<br />
-                정기 위생 교육을 이수했어요.<br />
-                실제 등록된 사업자로 인증했어요.<br />
-                공용 공간이 아닌 별도 주방이에요.
-            </div>
             </div>
         </div>
         </div>
